3bca1e3f4a0020c95380cea43a783e9fdf98dc85
ezyxip <anzej2003@gmail.com>
2024-07-21 23:03:17 +0600

    Работа над аргументами командной строки

---
+++ src/h/app.h
@@ -0,0 +1,10 @@
+#ifndef APP_H
+#define APP_H
+
+#include <string>
+
+bool check_input_file(std::string filepath);
+bool check_output_file(std::string filepath);
+bool check_filter_file(std::string filepath);
+
+#endif
